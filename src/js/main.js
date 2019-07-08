c6b81893c259bd32880b4e2b1afb4199fe23c9b8
Darek <my_email@example.com>
2019-07-08 19:43:31 +0200

    feat: move my homepage to the homepage-gulp

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,16 @@
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+//alert("co jest?");
+console.log('siema');
+const name = 'maciek';
+let isHappy = false;
+console.log(name, isHappy);
+console.log(`nazywam się ${name} 
+i to nie jest ${isHappy}`);
+
+console.log('nazywam się ' + name + ' i to nie jest ' +
+   isHappy + '-> stary sposób');
+
+console.log(name[0], name[1]);
